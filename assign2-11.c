beeac9882f984a3c64be6de1b655b764a96d5b89
gayatri444 <gayatriraykar09@gmail.com>
2025-06-25 18:18:32 +0530

    basic code

---
+++ assign2-11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter 2 numbes to the compare them:");
+    scanf("%d%d",&a,&b);
+    if (a == b)
+    printf("\n %d %d are equal :",a,b);
+else if(a>b)
+printf("\n%d is greater than %d",a,b);
+else if (a < b)
+printf("\n %d is less than %d",a,b);
+return 0;
+
+}
---
+++ assign2-8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a Lowercase Character:");
+    scanf("%c",&ch);
+    if(ch == 'a' || ch == 'i' || ch == 'o' || ch == 'u')
+    printf("%c is an vowel",ch);
+else 
+printf("%c are no vowel in your character",ch);
+return 0;
+
+}
